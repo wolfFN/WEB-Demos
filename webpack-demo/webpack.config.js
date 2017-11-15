a234c71168e9e18f9a88da2702c944a75fc14c28
Theo Zhang <wjzhang@amazon.com>
2017-11-15 23:38:30 +0800

    add react and webpack demo

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,52 +1,30 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    devtool: 'source-map',//配置生成Source Maps，选择合适的选项
-    entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
-    output: {
-        path: __dirname + "/public",//打包后的文件存放的地方
-        filename: "bundle.js"//打包后输出文件的文件名
+    entry: {
+        app: './src/index.js'
     },
-
-    module: {
-        loaders: [
-            {
-                test: /\.json$/,
-                loader: "json"
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-            {
-                test: /\.css$/,
-                loader: 'style!css'
-            },
-            {
-                test: /\.less$/,
-                loader: 'style!css!less'
-            }
-        ]
-
-    },
-
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin(),
-        new ExtractTextPlugin("style.css")
-    ],
-
+    devtool: 'inline-source-map',
     devServer: {
-        contentBase: "./public",//本地服务器所加载的页面所在的目录
-        port: 8888,
-        colors: true,//终端中输出结果为彩色
-        historyApiFallback: true,//不跳转
-        inline: true//实时刷新
+        contentBase: './dist'
+        // hot: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new UglifyJSPlugin()
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     }
-
-}
+};
